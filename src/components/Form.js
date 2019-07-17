2fdd7703753ca253f0056d949dcc301aff78f88b
levymikael <levymikael@gmail.com>
2019-07-17 13:00:07 +0300

    form try

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,11 +1,28 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Form extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Form</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+
+        <div class="section-title">
+          <h3>Where are you going ?</h3>
+          <input type="radio" name="destination-type" value="Work" />
+          Work
+          <input type="radio" name="destination-type" value="Gym" />
+          Gym
+          <input type="radio" name="destination-type" value="Out" />
+          Partying
+        </div>
+        <div class="section-title">
+          <h3>How long?</h3>
+          <input type="range" name="duration" min="0" max="2" />
+          {/* 0 - trip less than 30 mins
+                    1 - 30 mins to 1hour
+                    2 - 1 hour + */}
+        </div>
+      </div>
+    );
+  }
 }
