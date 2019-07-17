72f1c3392fe1ee6dfce525a2084e8bfecf257ee7
levymikael <levymikael@gmail.com>
2019-07-17 12:38:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import Form from './components/Form'
+import Playlist from './components/Playlist';
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      form : true
+    }
+  }
+  render() {
+    return (
+      <div className="App">
+        {
+          this.state.form ? 
+          <Form></Form>
+          :
+          <Playlist></Playlist>
+        }
+        <button onClick = {(e) => this.setState({form : !this.state.form})}>Switch</button>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Playlist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Playlist extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Playlist</h1>
+            </div>
+        )
+    }
+}
